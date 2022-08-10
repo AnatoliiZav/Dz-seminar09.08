be8e509878225d21ecb793cb475ae41edb262d2b
Anatolii <zavorinanatolii@mail.ru>
2022-08-11 01:35:17 +0800

    10 задание

--- Dz-10/Program.cs
+++ Dz-10/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число : ");
+int N1 = int.Parse(Console.ReadLine());
+
+if (N1 > 99 && N1 <1000) 
+    {
+         int a1 = N1 / 10;
+        int a2 = a1 % 10;
+        int result = a2;
+        
+/* / - целочисленный остаток( 123 - 1), % - дробная часть(123 - 3) */
+
+        Console.Write(result);
+    }
+        
+else
+{Console.Write("не трехзначное число"); 
+} 
+
