d937b1c84fbbfaea5b4abf7928c58ea6349f55d6
Anatolii <zavorinanatolii@mail.ru>
2022-08-11 02:04:44 +0800

    13 задание

---
+++ Dz-13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int N1 = int.Parse(Console.ReadLine());
+
+int a1 = N1 / 100;
+
+if (a1 ==0)
+{
+    Console.Write("третьей цифры нет");
+}
+int a2 = a1 % 10;
+int result = a2;
+Console.Write(result);
+    
